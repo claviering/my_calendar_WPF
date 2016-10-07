1a2b891e12b72f9a86c8678fc4947667d092cabc
weiye lin <weiye lin>
2016-10-07 11:36:36 +0800

    开出了右边的空白,在函数SetTexts里添加了一些看的懂的注释,第一次commot

---
+++ Calendar/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace Calendar
+{
+    public class Methods
+    {
+        private struct DWMMargins
+        {
+            public int top;
+            public int bottom;
+            public int left;
+            public int right;
+        }
+
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        private static extern void DwmExtendFrameIntoClientArea(IntPtr hwnd, ref DWMMargins margins);
+
+        public static void ExtendAeroToFullWindow(Window wnd)
+        {
+            IntPtr hwnd = new WindowInteropHelper(wnd).Handle;
+            wnd.Background = Brushes.Transparent;
+            HwndSource.FromHwnd(hwnd).CompositionTarget.BackgroundColor = Colors.Transparent;
+            DWMMargins m = new DWMMargins();
+            m.top = -1;
+            m.bottom = -1;
+            m.left = -1;
+            m.right = -1;
+            DwmExtendFrameIntoClientArea(hwnd, ref m);
+        }
+    }
+}
---
+++ Calendar/Window.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Calendar
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetTexts();
+            Left = SystemParameters.PrimaryScreenWidth - Width - 10;
+            Top = SystemParameters.PrimaryScreenHeight - Height - 50;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Methods.ExtendAeroToFullWindow(this);
+        }
+
+        private int myYear = DateTime.Now.Year, myMonth = DateTime.Now.Month;
+        
+        private void SetTexts()
+        {
+            DateTime tempDateTime = new DateTime(myYear, myMonth, 1); //获取当年当月的第一天
+            tempDateTime = tempDateTime.AddDays((int)tempDateTime.DayOfWeek == 0 ? -7 : -(int)tempDateTime.DayOfWeek);//计算第一个位置是那一天
+            for (int i = 10; i < myGrid.Children.Count; i++) //遍厉所有日期,改变他们的content,对应上日期
+            {             
+                ((Label)myGrid.Children[i]).Foreground = tempDateTime.Month == myMonth ? Brushes.Black : Brushes.Gray;//为日期上色
+                ((Label)myGrid.Children[i]).Content = tempDateTime.Day;
+                tempDateTime = tempDateTime.AddDays(1);//日期加1
+            }
+            txtCMonth.Text = myYear.ToString() + "-" + (myMonth < 10 ? "0" : "") + myMonth.ToString();//改变年月的内容
+        }
+
+        private void RightArrowClicked(object sender, MouseButtonEventArgs e)
+        {
+            myMonth++;
+            if(myMonth == 13)
+            {
+                myYear++;
+                myMonth = 1;
+            }
+            SetTexts();
+        }
+
+        private void LeftArrowClicked(object sender, MouseButtonEventArgs e)
+        {
+            myMonth--;
+            if (myMonth == 0)
+            {
+                myYear--;
+                myMonth = 12;
+            }
+            SetTexts();
+        }
+
+        private void NumMouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Label)sender).Background = new SolidColorBrush(Color.FromArgb(128, 125, 125, 125));
+        }
+
+        private void NumMouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Label)sender).Background = null;
+        }
+
+        private void NumMouseClick(object sender, MouseEventArgs e)
+        {
+            if (((Label)sender).Foreground == Brushes.Gray)
+            {
+                if ((int)((Label)sender).Content > 17)
+                    LeftArrowClicked(null, null);
+                else
+                    RightArrowClicked(null, null);
+            }
+        }
+
+        private void GoMouseEnter(object sender, MouseEventArgs e)
+        {
+            ((TextBlock)sender).Foreground = Brushes.Black;
+        }
+
+        private void GoMouseLeave(object sender, MouseEventArgs e)
+        {
+            ((TextBlock)sender).Foreground = Brushes.Gray;
+        }
+
+    }
+}
