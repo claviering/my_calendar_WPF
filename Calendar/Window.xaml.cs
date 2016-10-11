2e891541248086678d677cfc0e97c968da203a66
weiye lin <weiye lin>
2016-10-11 20:45:30 +0800

    成功显示新历假日

--- Calendar/Window.xaml.cs
+++ Calendar/Window.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.IO; //使用文件流
-using System.Collections.Generic; //使用List
 
 namespace Calendar
 {
@@ -13,7 +12,6 @@ namespace Calendar
         public MainWindow()
         {
             InitializeComponent();
-            set_dir_comboBox();
             SetTexts();
             show_lunar_day(DateTime.Now);
             Left = SystemParameters.PrimaryScreenWidth - Width - 10;
@@ -32,11 +30,14 @@ namespace Calendar
         private bool is_selected_day = false; //是否选中某一天
         private int current_day_of_children_Lable = 10;//当前天数是第几个Label
         Notes one_note = new Notes();
+        Holiday holiday = new Holiday();
+        private string holiday_string = "";
         #endregion
 
         #region 绘制日历
         private void SetTexts()
         {
+            holiday_string = ""; //初始化假日
             DateTime tempDateTime = new DateTime(myYear, myMonth, 1); //获取当年当月的第一天
             tempDateTime = tempDateTime.AddDays((int)tempDateTime.DayOfWeek == 0 ? -7 : -(int)tempDateTime.DayOfWeek);//计算第一个位置是那一天
             for (int i = 10; i < myGrid.Children.Count; i++) //遍厉所有日期,改变他们的content,对应上日期
@@ -59,6 +60,11 @@ namespace Calendar
                     ((Label)myGrid.Children[i]).Background = Brushes.White;
                 #endregion
 
+                #region 计算新历假日,返回string
+                if (tempDateTime.Month == myMonth)
+                    holiday_string += holiday.Fete(tempDateTime.Month,tempDateTime.Day);
+                #endregion
+
                 ((Label)myGrid.Children[i]).Content = tempDateTime.Day;//改变他们的content
                 tempDateTime = tempDateTime.AddDays(1);//日期加1
 
@@ -74,8 +80,15 @@ namespace Calendar
                 Directory.SetCurrentDirectory(".."); //切换目录
                 #endregion
 
+                
             }
             txtCMonth.Text = myYear.ToString() + "-" + (myMonth < 10 ? "0" : "") + myMonth.ToString();//改变年月的内容
+            
+            #region 显示新历节假日
+            textBox1.Text = holiday_string;
+            textBox1.BorderThickness = new Thickness(1.0);
+            textBox1.BorderBrush = Brushes.White;
+            #endregion
 
 
 
@@ -182,6 +195,7 @@ namespace Calendar
                 Notes show_note = new Notes();
                 string note_content = show_note.read_file(note_file_name);
                 textBox.Visibility = Visibility;
+                textBox1.Visibility = Visibility.Hidden;
                 image1.Visibility = Visibility;
                 image.Visibility = Visibility.Hidden;
                 image2.Visibility = Visibility;
@@ -190,11 +204,13 @@ namespace Calendar
             else
             {
                 textBox.Visibility = Visibility.Hidden;
+                textBox1.Visibility = Visibility;
                 image1.Visibility = Visibility.Hidden;
                 image.Visibility = Visibility;
                 image2.Visibility = Visibility.Hidden;
             }
             #endregion
+
         }
 
         private void GoMouseEnter(object sender, MouseEventArgs e)
@@ -274,6 +290,7 @@ namespace Calendar
         {
             textBox.Text = ""; //初始化textBox
             textBox.Visibility = Visibility;
+            textBox1.Visibility = Visibility.Hidden;
             image1.Visibility = Visibility; //显示保存
             image.Visibility = Visibility.Hidden; //隐藏加
             image2.Visibility = Visibility;
@@ -286,6 +303,7 @@ namespace Calendar
         {
             #region 控件的隐藏,显示
             textBox.Visibility = Visibility.Hidden;
+            textBox1.Visibility = Visibility;
             image.Visibility = Visibility; //显示保存
             image1.Visibility = Visibility.Hidden; //隐藏加
             image2.Visibility = Visibility.Hidden;
@@ -339,6 +357,7 @@ namespace Calendar
             Directory.SetCurrentDirectory(".."); //切换目录
 
             textBox.Visibility = Visibility.Hidden;
+            textBox1.Visibility = Visibility;
             image2.Visibility = Visibility.Hidden;
             image1.Visibility = Visibility.Hidden;
             image.Visibility = Visibility;
@@ -350,15 +369,164 @@ namespace Calendar
         }
         #endregion
 
-        private void set_dir_comboBox()
-        {
-            List<string> data = new List<string>();
-            data.Add("节假日");
-            comboBox.ItemsSource = data;
-            comboBox.SelectedIndex = 0;
-        }
 
     }
+    #region Holiday类
+    class Holiday
+    {
+        #region 节日数据
+        //国历节日*表示放假日
+        private static string[] sFtv = {"01 01 元旦","02 02 世界湿地日","02 10 国际气象节","02 14 情人节","03 01 国际海豹日","0 303 全国爱耳日",
+                                      "03 05 学雷锋纪念日","03 08 妇女节","03 12 植树节孙中山逝世纪念日","03 14 国际警察日","03 15 消费者权益日",
+                                      "03 17 中国国医节国际航海日","03 21 世界森林日消除种族歧视国际日世界儿歌日","03 22 世界水日",
+                                      "03 23 世界气象日","03 24 世界防治结核病日","03 30 巴勒斯坦国土日","04 01 愚人节全国爱国卫生运动月(四月) 税收宣传月(四月)",
+                                     "04 07 世界卫生日",
+                                     "04 22 世界地球日",
+                                     "04 23 世界图书和版权日",
+                                     "04 24 亚非新闻工作者日",
+                                     "05 01 劳动节",
+                                     "05 02 劳动节假日",
+                                     "05 03 劳动节假日",
+                                     "05 04 青年节",
+                                     "05 05 碘缺乏病防治日",
+                                     "05 08 世界红十字日",
+                                     "05 12 国际护士节",
+                                     "05 15 国际家庭日",
+                                     "05 17 国际电信日",
+                                     "05 18 国际博物馆日",
+                                     "05 20 全国学生营养日",
+                                     "05 23 国际牛奶日",
+                                     "05 31 世界无烟日",
+                                     "06 01 国际儿童节",
+                                     "06 05 世界环境保护日",
+                                     "06 06 全国爱眼日",
+                                     "06 17 防治荒漠化和干旱日",
+                                     "06 23 国际奥林匹克日",
+                                     "06 25 全国土地日",
+                                     "06 26 国际禁毒日",
+                                     "07 01 香港回归纪念日中共诞辰世界建筑日",
+                                     "07 02 国际体育记者日",
+                                     "07 07 抗日战争纪念日",
+                                     "07 11 世界人口日",
+                                     "07 30 非洲妇女日",
+                                     "08 01 建军节",
+                                     "08 08 中国男子节(爸爸节)",
+                                     "08 15 抗日战争胜利纪念",
+                                     "09 03 中国抗日战争胜利纪念日",
+                                     "09 08 国际扫盲日国际新闻工作者日",
+                                     "09 10 中国教师节",
+                                     "09 14 世界清洁地球日",
+                                     "09 16 国际臭氧层保护日",
+                                     "09 18 九·一八事变纪念日",
+                                     "09 20 国际爱牙日",
+                                     "09 27 世界旅游日",
+                                     "09 28 孔子诞辰",
+                                     "10 01 国庆节",
+                                     "10 02 国庆节",
+                                     "10 03 国庆节",
+                                     "10 04 世界动物日",
+                                     "10 06 老人节",
+                                     "10 08 全国高血压日世界视觉日",
+                                     "10 09 世界邮政日万国邮联日",
+                                     "10 10 辛亥革命纪念日世界精神卫生日",
+                                     "10 13 世界保健日国际教师节",
+                                     "10 14 世界标准日",
+                                     "10 15 国际盲人节(白手杖节)",
+                                     "10 16 世界粮食日",
+                                     "10 17 世界消除贫困日",
+                                     "10 22 世界传统医药日",
+                                     "10 24 联合国日",
+                                     "10 31 世界勤俭日",
+                                     "11 07 十月社会主义革命纪念日",
+                                     "11 08 中国记者日",
+                                     "11 09 全国消防安全宣传教育日",
+                                     "11 10 世界青年节",
+                                     "11 11 国际科学与和平周(本日所属的一周)",
+                                     "11 12 孙中山诞辰纪念日",
+                                     "11 14 世界糖尿病日",
+                                     "11 17 国际大学生节世界学生节",
+                                     "11 20 彝族年",
+                                     "11 21 彝族年世界问候日世界电视日",
+                                     "11 22 彝族年",
+                                     "11 29 国际声援巴勒斯坦人民国际日",
+                                     "12 01 世界艾滋病日",
+                                     "12 03 世界残疾人日",
+                                     "12 05 国际经济和社会发展志愿人员日",
+                                     "12 08 国际儿童电视日",
+                                     "12 09 世界足球日",
+                                     "12 10 世界人权日",
+                                     "12 12 西安事变纪念日",
+                                     "12 13 南京大屠杀(1937年)纪念日！紧记血泪史！",
+                                     "12 20 澳门回归纪念",
+                                     "12 21 国际篮球日",
+                                     "12 24 平安夜",
+                                     "12 25 圣诞节"
+                                     };
+        //农历节日*表示放假日
+        private static string[] lFtv = {
+                                     "01 01 春节",
+                                     "01 02 春节",
+                                     "01 03 春节",
+                                     "01 15 元宵节",
+                                     "05 05 端午节",
+                                     "06 24 火把节",
+                                     "06 25 火把节",
+                                     "06 26 火把节",
+                                     "07 07 七夕情人节",
+                                     "07 15 中元节",
+                                     "08 15 中秋节",
+                                     "09 09 重阳节",
+                                     "12 08 腊八节",
+                                     "12 24 小年",
+                                     "01 00 除夕"
+                                      };
+        #endregion
+
+        #region 计算公历节日
+        ///<summary>
+        ///公历日期返回公历节假日
+        ///</summary>
+        ///<param name="month"></param>
+        ///<param name="day"></param>
+        ///<returns></returns>
+        public string Fete(int month, int day)
+        {
+            for (int i = 0; i < sFtv.Length; i++)
+            {
+                string st = sFtv[i];
+                string[] s = st.Split(' ');
+                if (Convert.ToInt32(s[0]) == month && Convert.ToInt32(s[1]) == day)
+                {
+                    return st + Environment.NewLine;
+                }
+            }
+            return "";
+        }
+        #endregion
+
+        #region 计算农历节日
+        ///<summary>
+        ///农历日期返回农历节日
+        ///</summary>
+        ///<param name="lunarMonth"></param>
+        ///<param name="Lunarday"></param>
+        ///<returns></returns>
+        public string LunarFete(int lunarMonth, int lunarDay)
+        {
+            for (int i = 0; i < lFtv.Length; i++)
+            {
+                string st = lFtv[i];
+                string[] s = st.Split(' ');
+                if (Convert.ToInt32(s[0]) == lunarMonth && Convert.ToInt32(s[1]) == lunarDay)
+                {
+                    return s[2];
+                }
+            }
+            return "";
+        }
+        #endregion
+    }
+    #endregion
     #region Notes类
     /// <summary>
     /// 属性:Label
